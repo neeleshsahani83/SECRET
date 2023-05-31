11ee54efd6a8239363c098fbb69db6ff2ad65a38
Neelesh Sahani <neeleshsahani83@gmail.com>
2023-05-31 12:47:28 +0530

    Add Environment Vars

---
+++ app.js
@@ -0,0 +1,133 @@
+//jshint esversion:6
+
+require('dotenv').config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+const encrypt = require("mongoose-encryption");
+
+const app = express();
+
+console.log(process.env.API_KEY);
+
+app.use(express.static("public"));
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({
+     extended: true
+}));
+
+// mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true});
+
+mongoose.connect("mongodb://127.0.0.1:27017/userDB", {useNewUrlParser: true});
+
+// mongoose.connect("mongodb://0.0.0.0:27017/userDB", {useNewUrlParser: true});
+
+const userSchema = new mongoose.Schema ({
+    email: String,
+    password: String
+});
+
+// const secret = "Thisisourlittlesecret.";
+userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ["password"] });
+// userSchema.plugin(encrypt, { secret: secret, encryptedFields: ["password"] });
+
+
+const User = new mongoose.model("User", userSchema);
+
+
+
+
+app.get("/", function(req, res){
+    res.render("home");
+});
+ 
+app.get("/login", function(req, res){
+    res.render("login");
+});
+ 
+app.get("/register", function(req, res){
+    res.render("register");
+});
+
+
+// app.post("/register", function(req, res) {
+//     const newUser = new User({
+//         email: req.body.username,
+//         password: req.body.password
+//     });
+    
+
+//     newUser.save(function(err){
+//         if(err) {
+//             console.log(err);
+//         } else {
+//             //if user is registered then only rendering the secret page
+//             res.render("Secrets");
+//         }
+//     });
+// });
+
+app.post("/register", function(req, res) {
+    const newUser = new User({
+      email: req.body.username,
+      password: req.body.password
+    });
+  
+    newUser.save()
+      .then(() => {
+        // If the user is registered, render the secret page
+        res.render("Secrets");
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+});
+
+// app.post("/login", function(req, res){
+//     const username = req.body.username;
+//     const password = req.body.password;
+
+//     //look in the user collection
+//     //model.findone
+//     User.findOne({email: username}, function(err, foundUser){
+//         if(err) {
+//             console.log(err);
+//         } else {
+//             if(foundUser) {
+//                 if(foundUser.password === password) {
+//                     res.render("Secrets");
+//                 }
+//             }
+//         }
+//     });
+// });
+
+app.post("/login", function(req, res){
+    const username = req.body.username;
+    const password = req.body.password;
+
+    //look in the user collection
+    //model.findOne
+    User.findOne({ email: username })
+        .then(foundUser => {
+            if (foundUser) {
+                if (foundUser.password === password) {
+                    res.render("Secrets");
+                }
+            }
+        })
+        .catch(err => {
+            console.log(err);
+        });
+});
+
+
+
+
+
+
+
+app.listen(3000, function() {
+     console.log("Server started on port 3000.");
+});
